abf836150eeeabb849867a455ac87eddab18e2ee
KitsuneTech1 <kitsunetechnologiesinc@gmail.com>
2025-07-14 12:33:20 -0500

    Rebuild application with user changes

--- src/composables/useAuth.js
+++ src/composables/useAuth.js
@@ -1,6 +1,11 @@
 import axios from 'axios'
 
+// talk to your API subdomain directly
+const api = axios.create({
+  baseURL: 'https://api.0xb.lol/api'
+})
+
 export async function exchangeDiscordCode(code) {
-  const { data } = await axios.post('/api/auth/discord/callback', { code })
+  const { data } = await api.post('/auth/discord/callback', { code })
   return data.token
 }
