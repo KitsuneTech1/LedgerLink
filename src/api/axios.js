89e9b164587dc6e8bcfe0c57ab7ee9a2bc35f159
KitsuneTech1 <kitsunetechnologiesinc@gmail.com>
2025-07-14 11:26:04 -0500

    Fix environment variables for Vite

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuthStore } from '../store/auth';
 
 const apiClient = axios.create({
-  baseURL: import.meta.env.VUE_APP_API_BASE_URL,
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
