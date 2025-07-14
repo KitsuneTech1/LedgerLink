32267ff9ccfe2d974fe8fc0f81c15c3e393e9380
KitsuneTech1 <kitsunetechnologiesinc@gmail.com>
2025-07-14 12:25:35 -0500

    1

--- src/composables/useAuth.js
+++ src/composables/useAuth.js
@@ -1,11 +1,6 @@
 import axios from 'axios'
 
-// Always target the API subdomain
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
-})
-
 export async function exchangeDiscordCode(code) {
-  const { data } = await api.post('/auth/discord/callback', { code })
+  const { data } = await axios.post('/api/auth/discord/callback', { code })
   return data.token
 }
