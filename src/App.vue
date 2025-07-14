445a6b076486b44ce8088f3e6207567f9d8a321a
KitsuneTech1 <kitsunetechnologiesinc@gmail.com>
2025-07-14 11:21:43 -0500

    Fix authentication redirect for GitHub Pages

--- src/App.vue
+++ src/App.vue
@@ -6,4 +6,17 @@
 
 <script setup>
 import DefaultLayout from './layouts/DefaultLayout.vue'
+import { onMounted } from 'vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+onMounted(() => {
+  const redirect = sessionStorage.redirect
+  if (redirect) {
+    delete sessionStorage.redirect
+    const url = new URL(redirect)
+    router.push(url.pathname + url.search)
+  }
+})
 </script>
